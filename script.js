faadc0680e98d2445046103845bdf8d27e2c9fbd
Artem <blohin4337@gmail.com>
2023-05-13 07:59:14 +0300

    test merge

---
+++ script.js
@@ -0,0 +1,9 @@
+const buttonSeven = document.querySelector(".calculator__button_seven");
+
+buttonSeven.addEventListener("click", function(){
+  alert("Вы нажали на кнопку 7");
+});
+
+function func() {
+    alert("7");
+}
